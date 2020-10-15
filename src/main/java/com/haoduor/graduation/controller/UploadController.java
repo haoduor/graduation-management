f4388d74705b00a18823546cc8f7ed4e456bd254
haoduor <954040956@qq.com>
2020-10-15 10:34:31 +0800

    临时提交

--- src/main/java/com/haoduor/graduation/controller/UploadController.java
+++ src/main/java/com/haoduor/graduation/controller/UploadController.java
@@ -1,7 +1,24 @@
 package com.haoduor.graduation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/import")
 public class UploadController {
+
+    @Autowired
+    private HttpServletResponse response;
+
+    @Autowired
+    private HttpServletRequest request;
+
+
+
+
 }
