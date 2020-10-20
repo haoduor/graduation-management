72c7b3417e8fd20b31d32b14be19dc1992c25906
haoduor <954040956@qq.com>
2020-10-20 14:14:25 +0800

    新增教师接口

--- src/main/java/com/haoduor/graduation/controller/TeacherController.java
+++ src/main/java/com/haoduor/graduation/controller/TeacherController.java
@@ -3,25 +3,22 @@ package com.haoduor.graduation.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.haoduor.graduation.service.TeacherService;
+import com.haoduor.graduation.vo.BaseMessage;
 import com.haoduor.graduation.vo.PageMessage;
 import com.haoduor.graduation.vo.TeacherVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/teacher")
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
 
-    @ResponseBody
     @GetMapping("/list")
     public PageMessage getTeacher(@RequestParam(defaultValue = "1") int page,
                                   @RequestParam(defaultValue = "30") int pageSize) {
@@ -37,6 +34,16 @@ public class TeacherController {
         pageMessage.setData(res);
 
         return pageMessage;
+    }
+
+    @PostMapping("/add")
+    public BaseMessage add(TeacherVo teacherVo) {
+        return null;
+    }
+
+    @PostMapping("/set")
+    public BaseMessage set(TeacherVo teacherVo) {
+        return null;
     }
 
 }
