8c2bc7c4c39729265184a5eb80aade4f25f7d4ea
haoduor <954040956@qq.com>
2020-10-26 20:11:08 +0800

    修复公告id 失去精度的问题

---
+++ src/main/java/com/haoduor/graduation/controller/ChosenSubjectController.java
@@ -0,0 +1,11 @@
+package com.haoduor.graduation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chosen")
+public class ChosenSubjectController {
+
+
+}
