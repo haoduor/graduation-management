47882c42b7fbfaa3f5ac82ce55393caf79533d23
haoduor <954040956@qq.com>
2020-10-27 20:24:14 +0800

    修复全局long类型丢失精度

--- src/main/java/com/haoduor/graduation/config/JsonConfig.java
+++ src/main/java/com/haoduor/graduation/config/JsonConfig.java
@@ -1,12 +1,20 @@
 package com.haoduor.graduation.config;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializeFilter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.apache.shiro.crypto.hash.Hash;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JsonConfig {
@@ -17,6 +25,15 @@ public class JsonConfig {
 
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+
+        // 修复long 失丢失精度的问题
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+
+        fastJsonConfig.setSerializeConfig(serializeConfig);
 
         fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
 
