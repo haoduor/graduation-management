22c55c5440072c9cf2fe35c28c07b9b23e099d85
HP <927049198@qq.com>
2020-10-29 09:26:40 +0800

    教师端选题页适配,学生审核页适配,分页bug修复 选题页,编辑选题,编辑标签,添加选题功能添加 登录页,管理员-学生-教师合并

--- src/main/resources/static/js/tableModel.js
+++ src/main/resources/static/js/tableModel.js
@@ -236,6 +236,95 @@ const tableModel = (() =>{
                 }
             });
         },
+
+
+        //教师
+        //教师的选题表行
+        getTeaTopicTableData(data){
+            let html = '';
+            return new Promise((resolve,reject)=> {
+                data.forEach((items, index) => {
+                    let dataDifficulty = items['difficult'] == '0'?'容易':items['difficult'] == '1'?'普通':items['difficult'] == '2'?'困难':'?';//难度
+                    let dataTime =tools.dateFormat(new Date(parseInt(items['createTime'])),'yyyy-MM-dd hh:mm:ss');//时间
+                    let dataTag = ``;//标签
+                    items['tags'].forEach((items, index)=>{
+                        dataTag += `
+                            <div class="nmTag">${items['name']}</div>
+                        `;
+                    });
+                    let dataColumn = `
+                    <div class="dateColumn">
+                        <p class="nmDate">${items['title']}</p>
+                        <p class="nmDate">${items['source']}</p>
+                        <p class="nmDate">${items['content']}</p>
+                        <p class="nmDate">${dataDifficulty}</p>
+                        <p class="nmDate">${dataTime}</p>
+                        <div class="nmDate" 
+                        style="display: flex;
+                        flex-flow: row wrap;
+                        justify-content: center;">${dataTag}</div>                  
+                        <div class="staticDate" 
+                        style="display: flex;
+                        flex-flow: row wrap;
+                        justify-content: center;
+                        width: 200px;">
+                            <el-button @click="showEdit('${items['id']}',1)" type="text" icon="el-icon-edit">编辑选题</el-button>
+                            <el-button @click="showEdit('${items['id']}',2)" type="text" icon="el-icon-edit">编辑标签</el-button>
+                            <el-button @click="deleteList('${items['id']}')" type="text" icon="el-icon-delete">删除</el-button>
+                        </div>
+                     </div>
+                    `;
+                    html += dataColumn;
+                });
+                if(html != null){
+                    resolve(html);
+                }else{
+                    resolve('空页面');
+                }
+            });
+        },
+        //教师的学生选题审核
+        getStuAndSubject(data){
+            let html = '';
+            return new Promise((resolve,reject)=> {
+                data.forEach((items, index) => {
+                    let dataDifficulty = items['difficult'] == '0'?'容易':items['difficult'] == '1'?'普通':items['difficult'] == '2'?'困难':'?';//难度
+                    let dataTime =tools.dateFormat(new Date(parseInt(items['createTime'])),'yyyy-MM-dd hh:mm:ss');//时间
+                    let dataTag = ``;//标签
+                    items['tags'].forEach((items, index)=>{
+                        dataTag += `
+                            <div class="nmTag">${items['name']}</div>
+                        `;
+                    });
+                    let dataColumn = `
+                    <div class="dateColumn">
+                        <p class="nmDate">${items['title']}</p>
+                        <p class="nmDate">${items['source']}</p>
+                        <p class="nmDate">${items['content']}</p>
+                        <p class="nmDate">${dataDifficulty}</p>
+                        <div class="nmDate" 
+                        style="display: flex;
+                        flex-flow: row wrap;
+                        justify-content: center;">${dataTag}</div>      
+                         <p class="nmDate">${items['studentName']}</p>            
+                        <div class="staticDate" 
+                        style="display: flex;
+                        flex-flow: row wrap;
+                        justify-content: center;
+                        width: 200px;">
+                            <el-button @click="showEdit({sbjId:'${items['id']}',stuId:'${items['studentId']}'})" type="text" icon="el-icon-edit">通过审核</el-button>                        
+                        </div>
+                     </div>
+                    `;
+                    html += dataColumn;
+                });
+                if(html != null){
+                    resolve(html);
+                }else{
+                    resolve('空页面');
+                }
+            });
+        },
     }
 })();
 
