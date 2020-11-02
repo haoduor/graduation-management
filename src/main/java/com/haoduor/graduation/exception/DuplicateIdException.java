83263616ec7bac5da108b49a688b7df8bcc4b5c7
haoduor <954040956@qq.com>
2020-11-02 13:53:51 +0800

    修复存在最终选题的学生判断错误

--- src/main/java/com/haoduor/graduation/exception/DuplicateIdException.java
+++ src/main/java/com/haoduor/graduation/exception/DuplicateIdException.java
@@ -3,6 +3,10 @@ package com.haoduor.graduation.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * 重复id 错误
+ */
+
 @Data
 @AllArgsConstructor
 public class DuplicateIdException extends Exception{
--- src/main/java/com/haoduor/graduation/exception/ExcelFormatException.java
+++ src/main/java/com/haoduor/graduation/exception/ExcelFormatException.java
@@ -2,5 +2,8 @@ package com.haoduor.graduation.exception;
 
 import cn.hutool.poi.excel.ExcelUtil;
 
+/**
+ * excel 格式化错误
+ */
 public class ExcelFormatException extends Exception {
 }
